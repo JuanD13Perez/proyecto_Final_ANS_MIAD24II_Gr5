3aa6f08d36598c748c5e6e85abc44becdbf021bf
Juan Diego Pérez Sierra <diegojuk8@gmail.com>
2024-08-31 18:53:38 -0500

    feat: New Graphs

---
+++ aux_creador_dict.py
@@ -0,0 +1,31 @@
+import pickle
+
+dict_i = 
+{'FECHA_INSCRITO': 'datetime64[ns]',
+ 'INR_IDEN_NUMERO': 'str',
+ 'INR_NACI_FECHA': 'datetime64[ns]',
+ 'INR_GRADO': 'int64',
+ 'GRD_NOMBRE': 'str',
+ 'GRD_NIVEL_ESCOLAR': 'str',
+ 'D_SEXO': 'str',
+ 'INR_RES_CIUDAD': 'int64',
+ 'CIU_NOMBRE': 'str',
+ 'INR_RES_LOCALIDAD': 'int64',
+ 'LOC_NOMBRE': 'str',
+ 'ZON_NOMBRE': 'str',
+ 'BAR_NOMBRE': 'str',
+ 'UPZ_NOMBRE': 'str',
+ 'INR_DISCAPACIDAD': 'int64',
+ 'DIT_NOMBRE': 'str',
+ 'TAE_NOMBRE': 'str',
+ 'ETN_NOMBRE': 'str',
+ 'INR_ES_VICTIMA': 'int64',
+ 'PAI_NOMBRE': 'str',
+ 'INE_NOMBRE_INTERNO': 'str',
+ 'ASIGNADO_GRADO': 'float64',
+ 'ASIGNADO_OPCION': 'float64',
+ 'ASIGNADO_DANE_SEDE': 'str',
+ 'ASIGNADO_LOCALIDAD': 'str',
+ 'ASIGNADO_FECHA': 'datetime64[ns]'}
+
+pickle.dump(dict_i, open("dict_inscripciones.p", "wb"))
